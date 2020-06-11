09c2cf009cd7e21b33cbc164cf8be254a7a1a658
AnyOldName3 <krizdjali+github@gmail.com>
2020-06-11 17:33:28 +0100

    Make it build
    Requires at least CMake 3.18 RC1 due to a bug in older versions.
    
    Won't build on the command line via Umbrella/mob yet until it's been built at least once via Visual Studio or `msbuild -t:restore installer_omod.sln` is run manually.

---
+++ DummyCSFile.cs
@@ -0,0 +1,14 @@
+using OMODFramework;
+using OMODFramework.Scripting;
+
+namespace Dummy
+{
+    class Stub
+    {
+        static void Stub2()
+        {
+            OMOD omod = null;
+            IScriptFunctions scriptFunctions = null;
+        }
+    }
+}
