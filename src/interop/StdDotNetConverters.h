cbb6ab96b43d9653aabf6b06f6bef458744eab1c
AnyOldName3 <krizdjali+github@gmail.com>
2020-06-15 18:38:44 +0100

    Make toUTF8String available

--- src/interop/StdDotNetConverters.h
+++ src/interop/StdDotNetConverters.h
@@ -6,6 +6,8 @@ using namespace cli;
 
 std::wstring toWString(System::String^ string);
 
+std::string toUTF8String(System::String^ string);
+
 System::String^ toDotNetString(const std::wstring& wString);
 
 std::exception toStdException(System::Exception^ exception);
