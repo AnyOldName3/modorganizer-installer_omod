d701c13c5b034dc2889f26cdfb43cb34825703da
AnyOldName3 <krizdjali+github@gmail.com>
2020-07-10 01:16:30 +0100

    Add missing TODO

--- src/implementations/CodeProgress.cpp
+++ src/implementations/CodeProgress.cpp
@@ -7,6 +7,8 @@ void CodeProgress::Init(__int64 totalSize, bool compressing)
   mTotalSize = totalSize;
   mCompressing = compressing;
   MOBase::log::debug("CodeProgress::Init called with {} {}", totalSize, compressing);
+
+  // TODO: this
 }
 
 void CodeProgress::SetProgress(__int64 inSize, __int64 outSize)
