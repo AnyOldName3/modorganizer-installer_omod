add15c1eed22ef4d12cf076880b5034fc2a8d891
AnyOldName3 <krizdjali+github@gmail.com>
2020-08-04 15:56:40 +0100

    Work around https://github.com/erri120/OMODFramework/issues/31

--- src/installer_omod_en.ts
+++ src/installer_omod_en.ts
@@ -51,58 +51,58 @@
 <context>
     <name>OMODFrameworkWrapper</name>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="461"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="468"/>
         <source>Display Readme?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="463"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="470"/>
         <source>The Readme may explain installation options. Display it?&lt;br&gt;It will remain visible until you close it.</source>
         <extracomment>&lt;br&gt; is a line break. Translators can remove it if it makes things clearer.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="469"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="476"/>
         <source>%1 Readme</source>
         <extracomment>%1 is the mod name</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="266"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="273"/>
         <source>%1 wants to change [%2] %3 from &quot;%4&quot; to &quot;%5&quot;</source>
         <extracomment>%1 is the mod name [%2] is the ini section name. %3 is the ini setting name. %4 is the value already in Oblivion.ini. %5 is the value the mod wants to set.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="275"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="282"/>
         <source>%1 wants to set [%2] %3 to &quot;%4&quot;</source>
         <extracomment>%1 is the mod name [%2] is the ini section name. %3 is the ini setting name. %5 is the value the mod wants to set.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="278"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="285"/>
         <source>Update INI?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="321"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="328"/>
         <source>%1 installed and wants to activate the following plugins:&lt;ul&gt;&lt;li&gt;%2&lt;/li&gt;&lt;/ul&gt;However, it didn&apos;t try to activate these plugins:&lt;ul&gt;&lt;li&gt;%3&lt;/li&gt;&lt;/ul&gt;</source>
         <extracomment>%1 is the mod name &lt;ul&gt;&lt;li&gt;%2&lt;/li&gt;&lt;/ul&gt; becomes a list of ESPs and ESMs the OMOD installed and tried to activate. &lt;ul&gt;&lt;li&gt;%3&lt;/li&gt;&lt;/ul&gt; becomes a list of ESPs and ESMs the OMOD installed avoided activating. The point of this popup is to suggest which plugins the user might need to activate themselves.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="325"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="332"/>
         <source>OMOD didn&apos;t activate all plugins</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="348"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="355"/>
         <source>%1 has data for %2, but Mod Organizer 2 doesn&apos;t know what to do with it yet. Please report this to the Mod Organizer 2 development team (ideally by sending us your interface log) as we didn&apos;t find any OMODs that actually did this, and we need to know that they exist.</source>
         <extracomment>%1 is the mod name %2 is the name of a field in the OMOD&apos;s return data Hopefully this message will never be seen by anyone, but if it is, they need to know to tell the Mod Organizer 2 dev team.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="351"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="358"/>
         <source>Mod Organizer 2 can&apos;t completely install this OMOD.</source>
         <translation type="unfinished"></translation>
     </message>
